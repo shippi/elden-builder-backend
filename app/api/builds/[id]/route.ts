1a80edc12c6e4f0ad30701329e93666fc6cb6e20
David <hn94@students.waikato.ac.nz>
2024-04-12 23:15:53 +1200

    auth required implemented for post build

--- app/api/builds/[id]/route.ts
+++ app/api/builds/[id]/route.ts
@@ -12,25 +12,31 @@ export async function PUT(req: NextRequest, {params: {id}}: Props) {
     const {name, build, isPublic} = await req.json();
 
     try {
-        if (!id) NextResponse.json({"error": "Missing id"}, {status: 400});
-        
+      const authToken = req.headers.get("Authorization");
+      
+      if (!id || !authToken) return NextResponse.json({"error": "Missing id"}, {status: 400});
+      
+      const uid = await (await sql`SELECT uid FROM builds WHERE id=${id}`).rows[0].uid
+
+      if (authToken && await handleAuthToken(authToken, uid)) {
         await sql`UPDATE builds SET 
                     name=COALESCE(${name}, name),
                     build=COALESCE(${build}, build),
                     is_public=COALESCE(${isPublic}, is_public)
                   WHERE id=${id}`
-
         return NextResponse.json({"message": "Update/Save successful"}, {status: 200});
+      }
+      
+      return NextResponse.json({"error": "Access Token is invalid."}, {status: 500});
     }
     catch (error) {
-        return NextResponse.json({error}, {status: 500});
+      return NextResponse.json({error}, {status: 500});
     }
 }
 
 export async function DELETE(req: NextRequest, {params: {id}}: Props) {
   try {
     const authToken = req.headers.get("Authorization");
-    console.log(authToken);
     if (!id || !authToken) return NextResponse.json({"error": "Missing id or access token"}, {status: 400});
 
     const uid = await (await sql`SELECT uid FROM builds WHERE id=${id}`).rows[0].uid

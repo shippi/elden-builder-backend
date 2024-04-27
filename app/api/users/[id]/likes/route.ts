4177ff5f574da365fbf1f99ac38dfa71823155a4
David <hn94@students.waikato.ac.nz>
2024-04-28 01:24:11 +1200

    DELETE implemented for like

--- app/api/users/[id]/likes/route.ts
+++ app/api/users/[id]/likes/route.ts
@@ -1,3 +1,4 @@
+import { handleAuthToken } from "@/utils";
 import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,10 +12,37 @@ export async function POST(req: NextRequest, {params: {id}}: Props) {
     const { build_id } = await req.json();
 
     try {
-      await sql`INSERT into LIKES (build_id, user_id, created_at) values (${build_id}, ${id}, current_timestamp)`; 
-      return NextResponse.json({"Message": "Like successfully added"}, {status: 200});
+    	const authToken = req.headers.get("Authorization");
+
+      	if (!id || !authToken) return NextResponse.json({"error": "Missing id"}, {status: 400});
+      
+      	if (authToken && await handleAuthToken(authToken, id)){
+        	await sql`INSERT INTO likes (build_id, user_id, created_at) values (${build_id}, ${id}, current_timestamp)`; 
+        	return NextResponse.json({"Message": "Like successfully added"}, {status: 200});
+      	}
+        
+    	return NextResponse.json({"error": "Access Token is invalid."}, {status: 403});
     }
     catch (error) {
-      return NextResponse.json({error}, {status: 500});
+    	return NextResponse.json({error}, {status: 500});
+    }
+}
+
+export async function DELETE(req: NextRequest, {params: {id}}: Props) {
+	const { build_id } = await req.json();
+	try {
+    	const authToken = req.headers.get("Authorization");
+
+      	if (!id || !authToken) return NextResponse.json({"error": "Missing id"}, {status: 400});
+      
+      	if (authToken && await handleAuthToken(authToken, id)){
+        	await sql`DELETE FROM likes WHERE user_id=${id} AND build_id=${build_id}`; 
+        	return NextResponse.json({"Message": "Like successfully removed"}, {status: 200});
+      	}
+        
+    	return NextResponse.json({"error": "Access Token is invalid."}, {status: 403});
+    }
+    catch (error) {
+    	return NextResponse.json({error}, {status: 500});
     }
 }

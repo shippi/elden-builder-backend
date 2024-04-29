58ce9c1e69013b435c790cffd5e3bd2924d296fd
David <hn94@students.waikato.ac.nz>
2024-04-29 22:57:53 +1200

    removed yarn.lock, figured out deployment problem

--- utils/handleAuthToken.ts
+++ utils/handleAuthToken.ts
@@ -1,4 +1,5 @@
 import app from "@/lib/firebase";
+
 export async function handleAuthToken (accessToken: string, uid: string) {
     try {
         const authUser = await app.auth().verifyIdToken(accessToken.split(" ")[1]);

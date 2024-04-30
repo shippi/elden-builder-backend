ede98257dcd389b6a9a75fcf16a73fe1564c047b
David <hn94@students.waikato.ac.nz>
2024-04-30 22:49:23 +1200

    analytics sent when grabbing build info

--- app/api/builds/[id]/route.ts
+++ app/api/builds/[id]/route.ts
@@ -18,10 +18,11 @@ export async function GET(req: NextRequest, {params: {id}}: Props) {
     const decodedUid = await handleAuthToken(authToken);
 
     const row = await sql`
-    SELECT builds.*, users.username, COALESCE(view_count.count, 0) as views, COUNT(DISTINCT likes.user_id)::int as likes, 
+    SELECT builds.*, users.username, COALESCE(view_count.count, 0)::int as views, COUNT(DISTINCT likes.user_id)::int as likes, 
     CAST(COUNT(DISTINCT(CASE likes.user_id WHEN ${decodedUid} THEN 1 ELSE null END)) AS bit)::int as liked, 
     CAST(COUNT(DISTINCT(CASE bookmarks.user_id WHEN ${decodedUid} THEN 1 ELSE null END)) AS bit)::int as bookmarked FROM builds
                         LEFT JOIN users ON builds.uid = users.id
+                        FULL JOIN (SELECT build_id, COUNT(build_id) FROM views GROUP by build_id) view_count ON builds.id = view_count.build_id
                         FULL JOIN likes ON builds.id = likes.build_id
                         FULL JOIN bookmarks ON builds.id = bookmarks.build_id
                         WHERE builds.id = ${id}
